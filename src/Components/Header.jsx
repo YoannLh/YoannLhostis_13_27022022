945763dc08223ecb713caa2563cd0845a078d6d9
L'hostis <yolhostis@live.fr>
2022-02-28 21:46:13 +0100

    Header ok

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,45 @@
+import styled from 'styled-components'
+
+import logo from '../assets/argentBankLogo.png'
+import userLogo from '../assets/circle-user-solid.svg'
+
+const StyledHeader = styled.div`
+    display: flex;
+    width: 100%;
+    height: 65px;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const StyledLogo = styled.img`
+    height: 55px;
+    margin-left: 25px;
+`
+
+const StyledSignIn = styled.div`
+    display: flex;
+    margin-right: 25px;
+    align-items: center;
+`
+
+const StyledUserLogo = styled.img`
+    height: 20px;
+    margin-right: 5px;
+`
+
 function Header() {
-    return <div>Header</div>
+    const handleClick = () => {
+        console.log('click')
+    }
+    return (
+        <StyledHeader>
+            <StyledLogo src={logo} />
+            <StyledSignIn onClick={() => handleClick()}>
+                <StyledUserLogo src={userLogo} />
+                Sign in
+            </StyledSignIn>
+        </StyledHeader>
+    )
 }
 
 export default Header
