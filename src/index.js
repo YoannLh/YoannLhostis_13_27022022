8adab759e41e53d4c15926e4a8eb674f4fe123c6
L'hostis <yolhostis@live.fr>
2022-02-28 18:44:33 +0100

    Prettier, React-Router, Styled-Components, Pages, Header et Footer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import reportWebVitals from './reportWebVitals'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './Pages/Home'
+import Authentication from './Pages/Authentication'
+import Account from './Pages/Account'
+import Error from './Pages/Error'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 ReactDOM.render(
-  <React.StrictMode>
-    loulou
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/authentication" element={<Authentication />} />
+                <Route path="/account" element={<Account />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
