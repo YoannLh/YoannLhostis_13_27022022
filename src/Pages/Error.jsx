4136e4e956f03129a7528508d4831d1946ccbcef
L'hostis <yolhostis@live.fr>
2022-03-02 17:17:38 +0100

    Error ok

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,5 +1,26 @@
+import styled from 'styled-components'
+
+const StyledHero = styled.div`
+    display: flex;
+    width: 100%;
+    height: 560px;
+    background: rgb(19, 7, 43);
+`
+
+const StyledErrorMessage = styled.p`
+    margin: auto;
+    color: white;
+    font-size: 1.3em;
+`
+
 function Error() {
-    return <div>Error</div>
+    return (
+        <StyledHero>
+            <StyledErrorMessage>
+                Oups, cette page est introuvable :/
+            </StyledErrorMessage>
+        </StyledHero>
+    )
 }
 
 export default Error
